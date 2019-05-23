47906e963fafe1f92e42791440a272480e68c2ca
Nick <FireTime@gmail.com>
2019-05-23 16:26:59 -0400

    implement Hardware Servo control

--- SW/Arduino/ACC_Task_Schd/ACC_Task_Schd.ino
+++ SW/Arduino/ACC_Task_Schd/ACC_Task_Schd.ino
@@ -667,6 +667,7 @@ void driveHandler(char packetType, int value) {
 void setSteer(int us, int dly) {
   //steeringChannel.setDuty(us);
   myservo_steer.write(us);
+  PWM->PWM_CH_NUM[2].PWM_CDTYUPD = us;
   delay(dly);
 }
 
@@ -696,8 +697,8 @@ void setDrive(int us, int dly) {
     Serial.println(DistF);
   */
 //  myservo_drive.writeMicroseconds(us);
-    myservo_drive.write(us);
-
+//    myservo_drive.write(us);
+    PWM->PWM_CH_NUM[1].PWM_CDTYUPD = us;
 //  throttleChannel.setDuty(us);
   delay(dly);
 }
@@ -771,4 +772,31 @@ void ultrasonicChange_l()
     leftUltrasonic.set_pulse_dur(tUltrasonicEnd_l);
     //RisingFalling_Toggle = 0;
   }
+}
+
+void PWM_SERVO_SETUP()
+{
+    // PWM set-up on pins D38 and D36 for channels 1 and 2 respectively
+  REG_PMC_PCER1 |= PMC_PCER1_PID36;                  // Enable PWM 
+  REG_PIOC_ABSR |= PIO_ABSR_P6 | PIO_ABSR_P4;        // Set the port C PWM pins to peripheral type B
+  REG_PIOC_PDR  |= PIO_PDR_P6 | PIO_PDR_P4;          // Set the port C PWM pins to outputs
+  REG_PWM_CLK = PWM_CLK_PREA(0) | PWM_CLK_DIVA(42);  // Set the PWM clock A rate to 2MHz (84MHz/42)
+  
+
+  PWM->PWM_CH_NUM[1].PWM_CMR = PWM_CMR_CALG | PWM_CMR_CPRE_CLKA;     // Enable dual slope PWM and set the clock source as CLKA
+  PWM->PWM_CH_NUM[1].PWM_CPRD = 20000;                               // Set the PWM frequency 2MHz/(2 * 20000) = 50Hz;
+  PWM->PWM_CH_NUM[2].PWM_CMR = PWM_CMR_CALG | PWM_CMR_CPRE_CLKA;     // Enable dual slope PWM and set the clock source as CLKA
+  PWM->PWM_CH_NUM[2].PWM_CPRD = 20000;                               // Set the PWM frequency 2MHz/(2 * 20000) = 50Hz;
+  
+  REG_PWM_ENA = PWM_ENA_CHID2 | PWM_ENA_CHID1;                       //Enable PWM channels 1 and 2;
+
+  PWM->PWM_CH_NUM[1].PWM_CDTYUPD = 2000;                             // Set the PWM duty cycle to center / 50% / 1500 
+  PWM->PWM_CH_NUM[2].PWM_CDTYUPD = 2000;  
+  delay(100);
+  PWM->PWM_CH_NUM[1].PWM_CDTYUPD = 1000;                             // Set the PWM duty cycle to center / 50% / 1500 
+  PWM->PWM_CH_NUM[2].PWM_CDTYUPD = 1000;  
+  delay(100);
+  PWM->PWM_CH_NUM[1].PWM_CDTYUPD = 1500;                             // Set the PWM duty cycle to center / 50% / 1500 
+  PWM->PWM_CH_NUM[2].PWM_CDTYUPD = 1500;  
+  delay(100);
 }
