593a1063ca244a51cdcf9a129017c8fda22fb4d3
Nick <FireTime@gmail.com>
2019-05-23 16:38:23 -0400

    change init

--- SW/Arduino/ACC_Task_Schd/ACC_Task_Schd.ino
+++ SW/Arduino/ACC_Task_Schd/ACC_Task_Schd.ino
@@ -790,12 +790,6 @@ void PWM_SERVO_SETUP()
   
   REG_PWM_ENA = PWM_ENA_CHID2 | PWM_ENA_CHID1;                       //Enable PWM channels 1 and 2;
 
-  PWM->PWM_CH_NUM[1].PWM_CDTYUPD = 2000;                             // Set the PWM duty cycle to center / 50% / 1500 
-  PWM->PWM_CH_NUM[2].PWM_CDTYUPD = 2000;  
-  delay(100);
-  PWM->PWM_CH_NUM[1].PWM_CDTYUPD = 1000;                             // Set the PWM duty cycle to center / 50% / 1500 
-  PWM->PWM_CH_NUM[2].PWM_CDTYUPD = 1000;  
-  delay(100);
   PWM->PWM_CH_NUM[1].PWM_CDTYUPD = 1500;                             // Set the PWM duty cycle to center / 50% / 1500 
   PWM->PWM_CH_NUM[2].PWM_CDTYUPD = 1500;  
   delay(100);
