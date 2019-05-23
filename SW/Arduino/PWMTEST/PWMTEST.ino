9f8d66d13ca7809bfa5cbef17a9f46c7e06735bd
FireTime <FireTime@users.noreply.github.com>
2019-05-22 21:24:22 -0400

    added docs

--- SW/Arduino/PWMTEST/PWMTEST.ino
+++ SW/Arduino/PWMTEST/PWMTEST.ino
@@ -13,6 +13,54 @@
  * 2000 is max
  */
 
+
+
+/**
+ *    // PWM pins listed on Table 38-2 for Atmel ATSAM3X8E 
+      // datasheet available in www.atmel.com
+      // NOTE: only it is possible to use one pin for each
+      // PWM channel 
+      PA8 , // PWM_CH0 -> RX0
+      PB12, // PWM_CH0 -> SDA (SDA0)!
+      PC3 , // PWM_CH0 -> D35
+      PA21, // PWM_CH0 -> LED TX!
+      PB16, // PWM_CH0 -> DAC1 (audio)
+      PC2 , // PWM_CH0 -> D34
+
+      PA19, // PWM_CH1 -> D42
+      PB13, // PWM_CH1 -> SCL (SCL0)!
+      PC5 , // PWM_CH1 -> D37
+      PA12, // PWM_CH1 -> RX2
+      PB17, // PWM_CH1 -> AI8
+      PC4 , // PWM_CH1 -> D36 <--
+
+      PA13, // PWM_CH2 -> TX2
+      PB14, // PWM_CH2 -> D53 (SPISS)
+      PC7 , // PWM_CH2 -> D39
+      PA20, // PWM_CH2 -> D43
+      PB18, // PWM_CH2 -> AI9
+      PC6 , // PWM_CH2 -> D38 <--
+
+      PA9 , // PWM_CH3 -> TX0
+      PB15, // PWM_CH3 -> DAC0 (audio)
+      PC9 , // PWM_CH3 -> D41
+      PA0 , // PWM_CH3 -> CANTX
+      PB19, // PWM_CH3 -> AI10
+      PC8 , // PWM_CH3 -> D40
+
+      PC20, // PWM_CH4 -> na
+      PC21, // PWM_CH4 -> D9 (PWN)!
+
+      PC19, // PWM_CH5 -> D44
+      PC22, // PWM_CH5 -> D8 (PWN)!
+
+      PC18, // PWM_CH6 -> D45
+      PC23, // PWM_CH6 -> D7 (PWN)!
+
+      PC24  // PWM_CH7 -> D6 (PWN)!
+ */
+
+ 
 int i = 1500;
 int j = 1;
 void setup() {
@@ -52,3 +100,5 @@ void PWM_SERVO_SETUP()
   PWM->PWM_CH_NUM[1].PWM_CDTYUPD = 1500;                             // Set the PWM duty cycle to center / 50% / 1500 
   PWM->PWM_CH_NUM[2].PWM_CDTYUPD = 1500;                             // Set the PWM duty cycle to center / 50% / 1500            
 }
+
+
